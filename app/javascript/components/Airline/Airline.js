9efe369db1405c4241f15195489b4d5a9095f27f
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-07-27 19:21:43 +0100

    Installing Rails Router and setting up the App's routers

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Airline = () => {
+    return (
+        <div>
+            This is the view page for Airlines#Show
+        </div>
+    )
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Airlines = () => {
+    return (
+        <div>
+            This is the view page for Airlines#Index
+        </div>
+    )
+}
+
+export default Airlines
