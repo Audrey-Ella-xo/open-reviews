e259249c206006628f5348fd8d8046c8a219b5a9
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-07-28 11:04:59 +0100

    Use Airline component to dislay airline details on the index page

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Airline from './Airline'
 
 const Airlines = () => {
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(()=> {
+        axios.get('/api/v1/airlines.json')
+        .then( response => {
+            setAirlines(response.data.data)
+        } )
+        .catch( response => console.log(response) )
+    }, [airlines.length])
+
+    const grid = airlines.map( item => {
+        return (
+            <Airline 
+                key={item.attributes.name}
+                attributes={item.attributes}
+            />
+        )
+    })
+
     return (
-        <div>
-            This is the view page for Airlines#Index
+        <div className="home">
+            <div className="header">
+                <h1>OpenFlights</h1>
+                <div className="subheader">Honest, unbiased airline reviews.</div>
+            </div>
+            <div className="grid">
+                {grid}
+            </div>
         </div>
     )
 }
