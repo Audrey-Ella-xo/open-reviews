e1e2d82211e0c8947dfd4f0fc3f19c4e6cea8a1c
Audrey-Ella-xo <anitaudrey@gmail.com>
2021-07-29 16:31:04 +0100

    Handle submit and change methods

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -13,7 +13,7 @@ const ReviewForm = (props) => {
                     <input onChange={props.handleChange} value={props.review.title} type="text" name="title" placeholder="Review Title"/>
                 </div>
                 <div className='field'>
-                    <input onChange={props.handleChange} value={props.review.description} type="text" name="Description" placeholder="Review Description"/>
+                    <input onChange={props.handleChange} value={props.review.description} type="text" name="description" placeholder="Review Description"/>
                 </div>
                 <div className='field'>
                     <div className ="rating-container">
